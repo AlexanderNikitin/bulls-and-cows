390043124a5020a80719ec100b3f438182dd0635
Alexander Nikitin <alexnik000@yandex.ru>
2023-11-28 08:19:43 +0300

    Format

--- src/main/java/an/bac/Main.java
+++ src/main/java/an/bac/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String... args) throws IOException {
-        do{
+        do {
             new BullAndCows().play();
         } while (new ConsolePlayAgainQuestion().ask());
     }
